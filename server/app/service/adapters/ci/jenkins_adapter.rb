5b5716f27213036ccb2deb60fffc0907c3cac9ea
Silvio Montanari <montanari.silvio@gmail.com>
2012-11-05 12:48:12 +1100

    added jenkins adapter to invoke Jenkins remote api

--- server/app/service/adapters/ci/jenkins_adapter.rb
+++ server/app/service/adapters/ci/jenkins_adapter.rb
@@ -1,7 +1,46 @@
+require 'open-uri'
+require 'nokogiri'
+
 module Jashboard
   module CIServer
     class JenkinsAdapter
+      def fetch_build_info(settings)
+        @base_url = "http://#{settings.hostname}:#{settings.port}/job/#{settings.build_id}"
+        @doc = Nokogiri::XML(open("#{@base_url}/lastSuccessfulBuild/api/xml"))
+        Runtime::BuildInfo.new(get_time, get_duration, get_result, get_current_status)
+      end
 
+      private
+
+      def get_time
+        seconds = @doc.at_css("timestamp").
+                  text.
+                  to_i / 1000
+        Time.at(seconds).strftime("%d-%m-%Y %H:%M:%S")
+      end
+
+      def get_duration
+        @doc.at_css("duration").
+          text.
+          to_i / 1000
+      end
+
+      def get_result
+        @doc.at_css("result").text == "SUCCESS"
+      end
+
+      def get_current_status
+        last_build_number = @doc.at_css("number").text.to_i
+        status = nil
+        begin
+          open("#{@base_url}/#{last_build_number + 1}/api/xml")
+          status = 1
+        rescue OpenURI::HTTPError => error
+          status = 0 if error.message.start_with? "404"
+        rescue
+        end
+        status
+      end
     end
   end
 end

766daff21d0eeacaee9f60e4e23255352512c87c
Silvio Montanari <montanari.silvio@gmail.com>
2013-02-08 07:58:40 +1100

    using sinon or functional tests

--- web/test/funcunit/test_scenario_loader.js
+++ web/test/funcunit/test_scenario_loader.js
@@ -6,6 +6,7 @@
     steal.dev.log("Loading test scenario: " + scenarioName);
     steal(
       { src: 'jquery/dom/fixture', ignore: true },
+      { src: 'lib/sinon-1.5.2.js', ignore: true },
       { src: 'test/funcunit/angular_test_helper.js', ignore: true },
       { src: 'test/funcunit/scenarios/scenario_helper.js', ignore: true }
     ).then("test/funcunit/scenarios/" + scenarioName + ".js");

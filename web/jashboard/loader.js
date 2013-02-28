565290b9fd29cf349c0166c0dce647f8782f2e51
Silvio Montanari <montanari.silvio@gmail.com>
2013-03-01 08:17:42 +1100

    upgraded to jquery-ui-1.10.1

--- web/jashboard/loader.js
+++ web/jashboard/loader.js
@@ -1,14 +1,14 @@
 steal.dev.log("Loading 3rd party assets");
 steal(
   { src: "css/bootstrap.min.css", packaged: false },
-  { src: "css/jquery-ui/jquery-ui-1.9.1.custom.min.css", packaged: false },
+  { src: "css/jquery-ui/jquery-ui-1.10.1.custom.min.css", packaged: false },
   "jquery"
 ).then(
   { src: 'lib/angular.min.js', packaged: false },
   { src: 'lib/underscore-min.js', packaged: false },
   { src: 'lib/bootstrap.min.js', packaged: false },
   { src: 'lib/jquery.blockUI.js', packaged: false },
-  { src: 'lib/jquery-ui-1.9.1.custom.min.js', packaged: false }
+  { src: 'lib/jquery-ui-1.10.1.custom.min.js', packaged: false }
 );
 
 

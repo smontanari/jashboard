ea126ff77c3e0f5c7bd66129398a6eaaae79ae94
Silvio Montanari <montanari.silvio@gmail.com>
2013-03-12 16:21:37 +1100

    typo in scenario

--- web/test/funcunit/scenarios/create_dashboard.js
+++ web/test/funcunit/scenarios/create_dashboard.js
@@ -3,7 +3,7 @@ steal("test/funcunit/scenarios/display_dashboards_data.js");
 $.fixture("POST /ajax/dashboard", function(ajaxOriginalOptions, ajaxOptions, headers) {
   var data = JSON.parse(ajaxOptions.data);
 
-  if( "test new-dashboard" === data.name) {
+  if ("test new-dashboard" === data.name) {
     return [201, "success", {json: {id: "dashboard_4", name: "test new-dashboard", monitors: [] } }, {} ];
   }
   throw "unexpected data in the POST request: " + ajaxOptions.data;

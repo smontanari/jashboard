7b6d4bace1b0f4cdf44d71215be4bf6bb14335b4
Silvio Montanari <montanari.silvio@gmail.com>
2013-04-11 18:29:06 +1000

    plugin manager contains the adapters

--- server/app/plugins/plugin_manager.rb
+++ server/app/plugins/plugin_manager.rb
@@ -3,10 +3,19 @@ require 'plugins/monitor_adapter'
 module Jashboard
   module Plugin
     module PluginManager
-      def PluginManager.load_plugins(path = File.dirname(__FILE__))
+      @@adapters = {}
+      def self.load_plugins(path = File.dirname(__FILE__))
         Dir.glob("#{path}/**/*_plugin.rb") do |plugin_file|
           require plugin_file
         end
+      end
+      
+      def self.adapter_for_type(type)
+        @@adapters[type].new
+      end
+
+      def is_monitor_adapter_for_type(type)
+        @@adapters[type] = self
       end
 
       def is_monitor_plugin_for_type(type)

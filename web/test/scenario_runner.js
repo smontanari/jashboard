1d0d65897794bb9e01c701d9791331be8b7a5d7c
Silvio Montanari <montanari.silvio@gmail.com>
2014-03-30 20:48:47 +1100

    upgrading angularjs to 1.2.14

--- web/test/scenario_runner.js
+++ web/test/scenario_runner.js
@@ -1,11 +1,14 @@
-define(['bower_components/sinonjs/sinon', 'bower_components/smocker/smocker'], function() {
+define(['bower_components/sinonjs/sinon', 'bower_components/smocker/smocker.min'], function() {
   var regexp = /\?test_scenario=(\w+)/
   var match = regexp.exec(window.location.search);
   if (match) {
     var scenarioName = match[1];
     return function(callback) {
       console.warn("Loading test scenario: " + scenarioName);
-      require(["test/scenarios/" + scenarioName + "_scenario"], function(scenario) {
+      require(['test/scenarios/' + scenarioName + '_scenario'], function(scenario) {
+        smocker.play(function() {
+          this.get(/html\/.*/).forwardToServer();
+        });
         smocker.play(scenarioName);
         callback();
       });

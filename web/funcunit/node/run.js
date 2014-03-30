6702495fcebce7942b78877438d63d1037f43485
Silvio Montanari <montanari.silvio@gmail.com>
2014-03-30 13:57:29 +1100

    removing lib/jasmine, old funcunit

--- web/funcunit/node/run.js
+++
@@ -1,8 +0,0 @@
-var phantom = require('./phantom.js'),
-promise = require('promised-io/promise');
-
-phantom.run(process.argv[2]).then(function() {
-	process.exit();
-}, function() {
-	process.exit(1);
-});
